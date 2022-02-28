d8b55086a721360cd1af2286253f1d55a0d4c4a2
Woojin Oh <oh.woojin95@gmail.com>
2022-02-28 10:41:21 -0600

    removed for loop, updated variable names, moved declaration of generateRandomNum

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-let picGallery = {
+const oPICS = {
   1:'https://i.imgur.com/IaBx0k2.jpeg',
   2:'https://i.imgur.com/aez62JY.png',
   3:'https://i.imgur.com/NlyPUv0.png',
@@ -7,14 +7,13 @@ let picGallery = {
   6:'https://i.imgur.com/2lnuf9A.png'
 }
 
-const compareNumbers = () => {
-  const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-  let generatedNum = randomNum(1, 6);
-  const element = document.getElementById("myP");
-  for (let i = 1; i < 7; i++) {
-    if (generatedNum === i) {
-      let url = picGallery[i];
-      element.setAttribute('src', url);
-    }
+const generateRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const updateImg = () => {
+  let randomlyGeneratedNum = generateRandomNum(1, 6);
+  const image = document.getElementById("myImg");
+  if (randomlyGeneratedNum === 1 || randomlyGeneratedNum === 2 || randomlyGeneratedNum === 3 || randomlyGeneratedNum === 4 || randomlyGeneratedNum === 5 || randomlyGeneratedNum === 6) {
+    let urlOfImg = oPICS[randomlyGeneratedNum];
+    image.setAttribute('src', urlOfImg);
   }
 };
