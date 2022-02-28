d300bf931d0b7c4a413f7b22b252aa5c9e637b78
Woojin Oh <oh.woojin95@gmail.com>
2022-02-28 09:32:28 -0600

    muliple rolls, DRY, for loop

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,20 @@
+let picGallery = {
+  1:'https://i.imgur.com/IaBx0k2.jpeg',
+  2:'https://i.imgur.com/aez62JY.png',
+  3:'https://i.imgur.com/NlyPUv0.png',
+  4:'https://i.imgur.com/aPBcTgK.png',
+  5:'https://i.imgur.com/3J8kVFI.png',
+  6:'https://i.imgur.com/2lnuf9A.png'
+}
+
 const compareNumbers = () => {
   const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
   let generatedNum = randomNum(1, 6);
-  if (generatedNum === 1) {
   const element = document.getElementById("myP");
-  element.outerHTML = "<img src='https://i.imgur.com/IaBx0k2.jpeg'/>";
-  } else if ( generatedNum === 2) {
-    const element = document.getElementById("myP");
-  element.outerHTML = "<img src='https://i.imgur.com/aez62JY.png'/>";
-  } else if ( generatedNum === 3) {
-    const element = document.getElementById("myP");
-  element.outerHTML = "<img src='https://i.imgur.com/NlyPUv0.png'/>";
-  } else if ( generatedNum === 4) {
-    const element = document.getElementById("myP");
-  element.outerHTML = "<img src='https://i.imgur.com/aPBcTgK.png'/>";
-  } else if ( generatedNum === 5) {
-    const element = document.getElementById("myP");
-  element.outerHTML = "<img src='https://i.imgur.com/3J8kVFI.png'/>";
-  } else if ( generatedNum === 6) {
-    const element = document.getElementById("myP");
-  element.outerHTML = "<img src='https://i.imgur.com/2lnuf9A.png'/>";
-  } 
+  for (let i = 1; i < 7; i++) {
+    if (generatedNum === i) {
+      let url = picGallery[i];
+      element.setAttribute('src', url);
+    }
+  }
 };
-
-
