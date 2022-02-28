9a3ecb58762bf2e29dd6d0c1f1b54bcb72c4acf5
Woojin Oh <oh.woojin95@gmail.com>
2022-02-28 13:55:25 -0600

    css additions

--- JS/script.js
+++ JS/script.js
@@ -6,27 +6,13 @@ const oPICS = {
   5:'https://i.imgur.com/uaESiMS.png',
   6:'https://i.imgur.com/mPaO13v.png'
 }
-/*
-const generateRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const updateImg = () => {
-  let randomlyGeneratedNum = generateRandomNum(1, 6);
-  const image = document.getElementById("myImg");
-  const message = document.getElementById("message");
-  if (randomlyGeneratedNum === 1 || randomlyGeneratedNum === 2 || randomlyGeneratedNum === 3 || randomlyGeneratedNum === 4 || randomlyGeneratedNum === 5 || randomlyGeneratedNum === 6) {
-    let urlOfImg = oPICS[randomlyGeneratedNum];
-    image.setAttribute('src', urlOfImg);
-    message.innerHTML = randomlyGeneratedNum + "!";
-  }
-};
-*/
 
 const generateRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const rollDice = () => {
   let randomlyGeneratedNum = generateRandomNum(1, 6);
   const image = document.getElementById("myImg");
-  const message = document.getElementById("message");
+  const message = document.getElementById("result");
   const exclamation = document.getElementById("exclamation");
   
   let urlOfImg = oPICS[randomlyGeneratedNum];
